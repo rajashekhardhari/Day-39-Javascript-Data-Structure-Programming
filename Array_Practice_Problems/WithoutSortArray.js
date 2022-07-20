b9e9e7af6bd768abd751d828efbe031149720964
srajashekhar <udayarajashekhar@gmail.com>
2022-07-20 12:07:49 +0530

    Added: javaScript Array and Map Problems

---
+++ Array_Practice_Problems/WithoutSortArray.js
@@ -0,0 +1,52 @@
+let threeDigitNumbersArr = new Array();
+
+for(let i =0;i<10;i++)
+{
+    let number = Math.floor(Math.random()*999)+100;
+    threeDigitNumbersArr.push(number);
+}
+console.log(threeDigitNumbersArr);
+
+let secondLargest = threeDigitNumbersArr[0];
+let largest = threeDigitNumbersArr[0];
+let secondSmallest = threeDigitNumbersArr[0];
+let smallest = threeDigitNumbersArr[0];
+
+for(let i =1;i<threeDigitNumbersArr.length;i++)
+{
+    if(secondLargest<threeDigitNumbersArr[i])
+    {
+        if(largest<threeDigitNumbersArr[i])
+        {
+            secondLargest = largest;
+            largest = threeDigitNumbersArr[i] 
+        }
+        else
+        {
+            secondLargest = threeDigitNumbersArr[i];
+        }
+    }
+
+    if(secondSmallest>threeDigitNumbersArr[i])
+    {
+        if(smallest>threeDigitNumbersArr[i])
+        {
+            secondSmallest = smallest;
+            smallest = threeDigitNumbersArr[i] 
+        }
+        else
+        {
+            secondSmallest = threeDigitNumbersArr[i];
+        }
+    }
+}
+
+console.log("Second smallest value is : "+secondSmallest);
+console.log("Second largest value is : "+secondLargest);
+
+// Sort the array and find second largest & second smallest numbers
+
+let sortedArray = threeDigitNumbersArr.sort();
+
+console.log("Second smallest value using array is : "+sortedArray[1]);
+console.log("Second largest value using array is : "+sortedArray[sortedArray.length-2]);
